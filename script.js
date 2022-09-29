7113bd7587eb56cd39f60ef7ed3047f5c7e3ea31
tegarsetyo6 <tegarsetyo601@gmail.com>
2022-09-29 10:06:39 +0700

    finish calculator still need a little finishing

--- script.js
+++ script.js
@@ -5,23 +5,61 @@ class Calculator {
   }
 
   clear() {
-    this.currentOperandTextElement = '';
-    this.previousOperandTextElement = '';
+    currentOperandTextElement.innerText = '';
+    previousOperandTextElement.innerText = '';
     this.operation = undefined;
   }
 
-  delete() {}
+  delete() {
+    currentOperandTextElement.innerText = '';
+  }
 
   appendNumber(number) {
+    if (number === '.' && currentOperandTextElement.innerText.includes('.')) return;
     this.currentOperandTextElement.innerText = currentOperandTextElement.innerText.toString() + number.toString();
   }
 
-  chooseOperation(operation) {}
+  chooseOperation(operation) {
+    if (currentOperandTextElement.innerText === '') return;
+    if (previousOperandTextElement.innerText !== '') {
+      this.compute();
+    }
+    this.operation = operation;
+    previousOperandTextElement.innerText = currentOperandTextElement.innerText + ' ' + operation;
+    currentOperandTextElement.innerText = '';
+  }
 
-  compute() {}
+  compute() {
+    let computation;
+    const prev = parseFloat(previousOperandTextElement.innerText);
+    const current = parseFloat(currentOperandTextElement.innerText);
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (this.operation) {
+      case '+':
+        computation = prev + current;
+        break;
+      case '-':
+        computation = prev - current;
+        break;
+      case '*':
+        computation = prev * current;
+        break;
+      case '/':
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+
+    currentOperandTextElement.innerText = computation;
+    this.operation = undefined;
+    previousOperandTextElement.innerText = '';
+  }
 
   updateDisplay() {
-    this.currentOperandTextElement.innerText = this.currentOperandTextElement.innerText;
+    currentOperandTextElement.innerText = currentOperandTextElement.innerText;
+    previousOperandTextElement.innerText = previousOperandTextElement.innerText;
   }
 }
 
@@ -31,11 +69,29 @@ const equalsButton = $('.data-equals');
 const allClearButton = $('.all-clear-button');
 const clearButton = $('.clear-button');
 const currentOperandTextElement = document.getElementById('current-operand');
-const previousOperandTextElement = $('.previous-operand');
+const previousOperandTextElement = document.getElementById('previous-operand');
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
 numberButtons.click(function (event) {
   calculator.appendNumber(event.target.id);
   calculator.updateDisplay();
+});
+
+operationButtons.click(function (event) {
+  calculator.chooseOperation(event.target.id);
+  calculator.updateDisplay();
+});
+
+allClearButton.click(function () {
+  calculator.clear();
+});
+
+clearButton.click(function () {
+  calculator.delete();
+  calculator.updateDisplay();
+});
+
+equalsButton.click(function () {
+  calculator.compute(this.oper);
 });
